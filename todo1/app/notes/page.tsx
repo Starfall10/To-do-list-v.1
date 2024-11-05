6c20d7a37366cd9af5886a9426b0f5786a252f70
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2024-11-05 03:45:38 +0000

    Finsihed the barebones

--- todo1/app/notes/page.tsx
+++ todo1/app/notes/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import CreateNode from './CreateNode';
 // import styles from './Notes.module.css';
 
 type NoteType = {
@@ -9,7 +10,7 @@ type NoteType = {
 };
 
 async function getNotes(): Promise<NoteType[]> {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
+  const res = await fetch('http://127.0.0.1:8090/api/collections/Collection1/records?page=1&perPage=30');
   const data = await res.json();
   return data?.items as NoteType[];
 }
@@ -18,13 +19,15 @@ export default async function NotesPage() {
   const notes = await getNotes();
 
   return(
-    <div>
-      <h1>Notes</h1>
+    <div className='bg-white'>
+      <h1></h1>
       <div>
         {notes?.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </div>
+
+      <CreateNode />
     </div>
   );
 }
@@ -39,6 +42,7 @@ function Note({ note }: { note: NoteType }) {
         <h5>{content}</h5>
         <p>{created}</p>
       </div>
+      <br />
     </Link>
   );
 }
---
+++ todo1/pb_migrations/1730777400_updated_Collection1.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0pe2joiec0ehiql")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "6oifktj6",
+    "name": "title",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0pe2joiec0ehiql")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "6oifktj6",
+    "name": "field",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})

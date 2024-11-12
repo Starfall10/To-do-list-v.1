e6b9843cd1c1a9e6170633a5c8cee514ef30a588
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2024-11-12 07:03:45 +0000

    Added edit button

--- todo1/app/notes/components/Task.tsx
+++ todo1/app/notes/components/Task.tsx
@@ -1,15 +1,65 @@
+"use client";
+
 import { ITask } from "@/types/tasks";
-import React from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import React, { FormEventHandler, useState } from "react";
+import Model from "./Model";
+import { useRouter } from "next/navigation";
+import { editTodo } from "@/api";
 
 interface TaskProps {
   task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = React.useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] =
+    React.useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id, 
+      text: taskToEdit,
+    });
+    setTaskToEdit("");
+    router.refresh();
+    setOpenModalEdit(false);
+  };
+
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
-      <td>{task.id}</td>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit
+          onClick={() => {
+            setOpenModalEdit(true);
+          }}
+          cursor="pointer"
+          className="text-blue-500"
+          size={25}
+        />
+        <Model modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit task</h3>
+            <div className="modal-action">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+              <button type="submit" className="btn">
+                Add
+              </button>
+            </div>
+          </form>
+        </Model>
+        <FiTrash2 cursor="pointer" className="text-red-500" size={25} />
+      </td>
     </tr>
   );
 };
